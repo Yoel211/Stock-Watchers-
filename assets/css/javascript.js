4be02ba968bfb85a8e58189e19083240244ab3b7
reinerkp <kate.reiner1@gmail.com>
2023-02-09 20:39:46 -0700

    creating for loop for news articles and added addition stock informaiton in the first fetch

--- assets/css/javascript.js
+++ assets/css/javascript.js
@@ -3,7 +3,6 @@ var newsApiKey = "Y05JOHE1Z7ATCKW7";
 // Create variables for ticker and price API/ Finhub
 var tickerApiKey = "cfe7pg9r01qp08kufpagcfe7pg9r01qp08kufpb0";
 var priceApiKey = "cfe7pg9r01qp08kufpagcfe7pg9r01qp08kufpb0";
-var searchForm = document.querySelector("#search-form");
 var searchStock = document.querySelector("#searchStocks");
 
 var searchStock = document.querySelector("#searchStocks");
@@ -14,87 +13,99 @@ var searchForm = document.querySelector("#search-button");
 
 // Create a function to pull data from search button using the API ***USE ACT 24***
 let stockTicker = function (event) {
-    event.preventDefault();
-    let search = ticker.value;
-    if (search) {
-        console.log();
-    }
+  event.preventDefault();
+  let search = ticker.value;
+  if (search) {
+    console.log();
+  }
 
-    console.log(search)
+  console.log(search);
 
-    // Stock Name Data for stocks
-    fetch(
-        `https://finnhub.io/api/v1/search?q=${search}&token=${tickerApiKey}`
-    )
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('first stock', data)
-            //getStock(data);
-        })
-        .catch();
+  // Stock Name Data for stocks
+  fetch(`https://finnhub.io/api/v1/search?q=${search}&token=${tickerApiKey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("first stock", data);
+      //getStock(data);
+    })
+    .catch();
 
-    // Gets Price Data for Stocks
-    fetch(
-        `https://finnhub.io/api/v1/quote?symbol=${search}&token=${priceApiKey}`
-    )
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('stock price', data)
-            // getStock(data);
-            console.log(data.c);
-            // Dan says this needs to be in the scope, so I moved it here! 
-            var currentPrice = document.querySelector("#currentP");
-            currentPrice.textContent = `Current Price: ${data.c}`;
+  // Gets Price Data for Stocks
+  fetch(`https://finnhub.io/api/v1/quote?symbol=${search}&token=${priceApiKey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("stock price", data);
+      // getStock(data);
+      console.log(data.c);
+      // Dan says this needs to be in the scope, so I moved it here!
+      var currentPrice = document.querySelector("#currentP");
+      currentPrice.textContent = `Current Price: ${data.c}`;
 
-            var highP = document.querySelector("#highP");
-            highP.textContent = `High Price: ${data.h}`;
-        
-            var lowP = document.querySelector("#lowP");
-            lowP.textContent = `Low Price: ${data.l}`;
-        
-            var openP = document.querySelector("#openP");
-            openP.textContent = `Open Price: ${data.o}`;
-        
-            var prevClose = document.querySelector("#prevClose");
-            prevClose.textContent = `Previous Close: ${data.pc}`;
-        
+      var highP = document.querySelector("#highP");
+      highP.textContent = `High Price: ${data.h}`;
 
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+      var lowP = document.querySelector("#lowP");
+      lowP.textContent = `Low Price: ${data.l}`;
 
-    // Pulls News data for stock ticker
-    fetch(
-        `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${search}&apikey=${newsApiKey}`
-    )
+      var openP = document.querySelector("#openP");
+      openP.textContent = `Open Price: ${data.o}`;
 
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log('stock news', data)
-            getStock(data);
+      var prevClose = document.querySelector("#prevClose");
+      prevClose.textContent = `Previous Close: ${data.pc}`;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-    
+  // Pulls News data for stock ticker
+  fetch(
+    `https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=${search}&apikey=${newsApiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("stock news", data);
+      // getStock(data);
+      console.log(data.feed);
 
-        .catch();
+     
+      if (!data.feed) {
+        console.log("No Results");
+        search.innerHTML = '<h3>No results found, search again!</h3>';
+      } else {
+        search.innerHTML = '';
+        for ( var i = 0; i < 5; i ++ ) {
+             var articleEl = document.querySelector('.current-news')
+            var articleData = data.feed[i]
+            var articletitle = document.createElement('p')
+            var articleSummary = document.createElement('p')
+            articleSummary.textContent = 'Summary: ' + articleData.summary
 
-    console.log(data[0]);
-    // comment from Jesus - append ticker data into div
-    var currentSymbol = document.querySelector("#symbol");
-    currentSymbol.textContent = `Symbol: ${search}`;
+            articletitle.textContent = 'Title: ' + articleData.title
+            articleEl.append(articletitle, articleSummary)
+            
+        }
 
+      }
+    })
 
-    // variable to include ALL stock info?    
-    var stockInfo = document.querySelector(".current-prices");
+    .catch();
 
-    // function to render ALL current stock info 
-    /* function renderCurrent(weather) {
+  console.log(data[0]);
+  // comment from Jesus - append ticker data into div
+  var currentSymbol = document.querySelector("#symbol");
+  currentSymbol.textContent = `Symbol: ${search}`;
+
+  // variable to include ALL stock info?
+  var stockInfo = document.querySelector(".current-prices");
+
+  // function to render ALL current stock info
+  /* function renderCurrent(weather) {
         console.log(weather);
         cityName.textContent = weather.city.name;
         dateEl.textContent = `${weather.list[0].dt_txt}`;
@@ -104,8 +115,7 @@ let stockTicker = function (event) {
         windEl.textContent = `Wind: ${weather.list[0].wind.speed} MPH`;
         humidEl.textContent = `Humidity: ${weather.list[0].main.humidity}%`;
       } */
-
-
+};
 // Create event listeners for the search
 searchForm.addEventListener("click", stockTicker);
 
@@ -113,6 +123,4 @@ searchForm.addEventListener("click", stockTicker);
 // use local storage to save watchlist stocks
 
 // Create a function for populating news articles
-// Create a button that takes you to news articles 
-
-    })
+// Create a button that takes you to news articles
